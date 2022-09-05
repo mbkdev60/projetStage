a0580a3513827176b1b2ece428decf1c92363dcc
Moez Ben Khaled <bkmoez2410@gmail.com>
2022-09-05 16:22:43 +0200

    chore: clean-up

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 // import { GlobalStyle } from "./styles/global";
 import App from "./App";
 
+
 ReactDOM.render(
 	<React.StrictMode>
 		{/* <GlobalStyle /> */}
@@ -11,3 +12,10 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>
+// );
